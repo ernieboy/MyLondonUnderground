cf8c45ceaac780a6380a675addc2ae05ba47a633
Ernest Fakudze <ernie@ernieboy.com>
2017-09-30 17:53:29 +0100

    Adds more functionality to handler.

--- MyLondonUnderground.Application/CommandHandlers/ListTubeLinesCommandHandler.cs
+++ MyLondonUnderground.Application/CommandHandlers/ListTubeLinesCommandHandler.cs
@@ -3,12 +3,20 @@ using MediatR;
 using MyLondonUnderground.Application.Commands;
 using MyLondonUnderground.QueryStack.ViewModels;
 using System.Collections.Generic;
+using MyLondonUnderground.QueryStack.Abstractions;
 
 namespace MyLondonUnderground.Application.CommandHandlers
 {
     public class ListTubeLinesCommandHandler : 
         IAsyncRequestHandler<ListTubeLinesCommand, ICollection<TubeLinesListingViewModel>>
     {
+        private ILondonUndergroundDbReaderService _londonUndergroundDbReaderService;
+
+        public ListTubeLinesCommandHandler(ILondonUndergroundDbReaderService londonUndergroundDbReaderService)
+        {
+            _londonUndergroundDbReaderService = londonUndergroundDbReaderService;
+        }
+
         public Task<ICollection<TubeLinesListingViewModel>> Handle(ListTubeLinesCommand message)
         {
             throw new System.NotImplementedException();
